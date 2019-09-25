2fffc0127c974e71efe1aed586416ac979121bcd
Oleg Levantin <od@datalab.fit>
2019-09-25 19:18:17 +0300

    Add add_item_to_cart, add orders and shop

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
+        user_code: 320400377,
+        client_type: 'telegram', 
+
+        cart_id: null,
+        get_cart_id_url: "http://localhost:8080/get_cart_id", 
         categories: null,
         categories_url: "http://localhost:8080/get_categories",
         subcategories: null,
@@ -8,11 +13,20 @@ var app = new Vue({
         product_url: "http://localhost:8080/get_products",
         products: null,
         what_to_show: "categories",
+
+        quantity: [],
+        add_item_to_cart_url: "http://localhost:8080/add_item_to_cart",
+
+
     },
 
     methods: {
         get_categories: async function() {
             await axios.get(this.categories_url).then(response => (this.categories = response.data));
+        },
+        get_cart_id: async function() {
+            await axios.post(this.get_cart_id_url, {'user_code': this.user_code, 'client_type': this.client_type})
+            .then(response => this.cart_id = response.data["cart_id"]);
         },
         get_subcategories: async function(category_id) {
             await axios.post(this.subcategories_url, {'category_id': category_id})
@@ -20,13 +34,25 @@ var app = new Vue({
             this.what_to_show = 'subcategories'
         },
         get_products: async function(subcategory_id) {
-          await axios.post(this.product_url, {'subcategory_id': subcategory_id})
-          .then(response => this.products = response.data);
-          this.what_to_show = 'products'
+            await axios.post(this.product_url, {'subcategory_id': subcategory_id})
+            .then(response => this.products = response.data);
+            this.what_to_show = 'products'
+
+            this.quantity = []
+            for (let i in this.products) {
+                this.quantity.push(1)
+            }
         },
+        add_item_to_cart: async function(product_id, quantity) {
+            await axios.post(this.add_item_to_cart_url, {"product_id": product_id, 
+                                                "quantity": quantity, 
+                                                "cart_id": this.cart_id})
+            .then(response => console.log(response.data));
+        }
     },
 
-    created: function() {
-        this.get_categories();
+    created: async function() {
+        await this.get_categories();
+        await this.get_cart_id()
     }
   })
